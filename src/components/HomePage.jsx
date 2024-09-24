f1e827868d3e5bd2ee75be68ab1882f60f67a516
Chitra Savaj <csavaj@my.centennialcollege.ca>
2024-09-24 04:44:16 -0400

    git link added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -24,7 +24,7 @@ const HomePage = () => {
               <a href="https://www.instagram.com/savagesavaj?igsh=YzUyM2NtN3l3MjJs" target="_blank" rel="noopener noreferrer">
                 <FaInstagram className="border border-[#ff7a57] rounded-full p-2" />
               </a>
-              <a href="https://github.com/your-profile" target="_blank" rel="noopener noreferrer">
+              <a href="https://github.com/ChitraSavaj/Chitra-Savaj-React-Portfolio/tree/master" target="_blank" rel="noopener noreferrer">
                 <FaGithub className="border border-[#ff7a57] rounded-full p-2" />
               </a>
             </div>
